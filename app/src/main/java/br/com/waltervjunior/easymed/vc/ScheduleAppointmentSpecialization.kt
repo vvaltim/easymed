3712250246a8fb0e90f7a917282527e92c2f17e7
Walter Júnior <vvaltim@gmail.com>
2018-04-16 13:11:26 -0300

    Melhorias
    Implementação do SwipeRefresh da especialização.

--- app/src/main/java/br/com/waltervjunior/easymed/vc/ScheduleAppointmentSpecialization.kt
+++ app/src/main/java/br/com/waltervjunior/easymed/vc/ScheduleAppointmentSpecialization.kt
@@ -6,29 +6,59 @@ import android.util.Log
 import br.com.waltervjunior.easymed.adapter.SimpleCardAdapter
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.gson.Gson
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.sdk25.coroutines.onClick
 import org.jetbrains.anko.setContentView
 import org.jetbrains.anko.toast
 
 class ScheduleAppointmentSpecialization : Activity() {
+    //region <! Variaveis !>
     lateinit var ui : ScheduleAppointmentSpecializationUi
     var mSpecialization : ArrayList<Pair<String, String>> = ArrayList()
     var db = FirebaseFirestore.getInstance()
+    //endregion
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //region <! Criando o layout !>
         super.onCreate(savedInstanceState)
         ui = ScheduleAppointmentSpecializationUi()
         ui.setContentView(this)
+        //endregion
 
-        db.collection("specialization").get().addOnCompleteListener {task ->
-            if (task.isSuccessful){
-                for (document in task.result){
-                    val temp = Pair(document.id, document.data["name"] as String)
-                    mSpecialization.add(temp)
+        //region <! Eventos de atualizar !>
+        ui.swipeRefreshLayout.setOnRefreshListener {
+            updateSpecialization()
+        }
+        ui.swipeRefreshLayout.isRefreshing = true
+        updateSpecialization()
+        //endregion
+
+//        ui.recyclerView.onClick { handler ->
+//            toast(handler)
+//        }https://stackoverflow.com/questions/24471109/recyclerview-onclick
+    }
+
+    //region <! Atualizando as especializações !>
+    private fun updateSpecialization(){
+        doAsync {
+            db.collection("specialization").get().addOnCompleteListener {task ->
+                if (task.isSuccessful){
+                    for (document in task.result){
+                        val temp = Pair(document.id, document.data["name"] as String)
+                        mSpecialization.add(temp)
+                    }
+                    runOnUiThread {
+                        ui.recyclerView.adapter = SimpleCardAdapter(mSpecialization)
+                        ui.swipeRefreshLayout.isRefreshing = false
+                    }
+                } else {
+                    runOnUiThread {
+                        toast("Erro ao tentar pegar dados")
+                        ui.swipeRefreshLayout.isRefreshing = false
+                    }
                 }
-                ui.recyclerView.adapter = SimpleCardAdapter(mSpecialization)
-            } else {
-                toast("Erro ao tentar pegar dados")
             }
         }
     }
+    //endregion
 }
--- app/src/main/java/br/com/waltervjunior/easymed/vc/ScheduleAppointmentSpecializationUi.kt
+++ app/src/main/java/br/com/waltervjunior/easymed/vc/ScheduleAppointmentSpecializationUi.kt
@@ -1,15 +1,20 @@
 package br.com.waltervjunior.easymed.vc
 
+import android.support.v4.content.ContextCompat
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.StaggeredGridLayoutManager
+import br.com.waltervjunior.easymed.R
 import br.com.waltervjunior.easymed.component.ActionBar
 import br.com.waltervjunior.easymed.component.actionBar
 import br.com.waltervjunior.easymed.extension.generateViewId
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 class ScheduleAppointmentSpecializationUi : AnkoComponent<ScheduleAppointmentSpecialization>{
     lateinit var actionBar: ActionBar
+    lateinit var swipeRefreshLayout : SwipeRefreshLayout
     lateinit var recyclerView: RecyclerView
 
     override fun createView(ui: AnkoContext<ScheduleAppointmentSpecialization>) = with(ui){
@@ -18,10 +23,14 @@ class ScheduleAppointmentSpecializationUi : AnkoComponent<ScheduleAppointmentSpe
                 title.text = "Especialização"
                 show(backImageView)
             }
-
-            recyclerView = recyclerView {
+            swipeRefreshLayout = swipeRefreshLayout {
                 generateViewId()
-                layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+                setColorSchemeColors(ContextCompat.getColor(context, R.color.colorPrimary))
+
+                recyclerView = recyclerView {
+                    generateViewId()
+                    layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+                }
             }.lparams(matchParent, matchParent){
                 below(actionBar)
             }
